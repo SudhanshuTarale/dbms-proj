6842a792b9bb329b60a1f6ad451aa3127d3e90a1
Sudhanshu Tarale <sudhanshutarale@Sudhanshus-MacBook-Air.local>
2021-11-27 19:15:36 -0500

    flask

--- main.py
+++ main.py
@@ -8,5 +8,13 @@ def index():
     return app.send_static_file('index.html')
 
 
+@app.route('/home')
+def home():
+    return app.send_static_file('home-page.html')
+
+@app.route('/team-stats')
+def team():
+    return app.send_static_file('team-stats.html')
+
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
